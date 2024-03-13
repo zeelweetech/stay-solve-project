6bd412206c7976127ef8a4c57964850192808b82
zeelweetech <zeel.weetech@gmail.com>
2024-03-13 14:38:26 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import RtlLayout from "layouts/rtl";
+import AdminLayout from "layouts/admin";
+import AuthLayout from "layouts/auth";
+import SignIn from "views/auth/SignIn";
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<AuthLayout />} />
+      <Route path="/*" element={<AdminLayout />} />
+      {/* <Route path="rtl/*" element={<RtlLayout />} /> */}
+      <Route path="/" element={<Navigate to="/" replace />} />
+    </Routes>
+  );
+};
+
+export default App;
---
+++ src/routes.js
@@ -0,0 +1,67 @@
+import React from "react";
+
+// Admin Imports
+import MainDashboard from "views/admin/default";
+import NFTMarketplace from "views/admin/marketplace";
+import Profile from "views/admin/profile";
+import DataTables from "views/admin/tables";
+import RTLDefault from "views/rtl/default";
+
+// Auth Imports
+import SignIn from "views/auth/SignIn";
+
+// Icon Imports
+import {
+  MdHome,
+  MdOutlineShoppingCart,
+  MdBarChart,
+  MdPerson,
+  MdLock,
+} from "react-icons/md";
+
+const routes = [
+  {
+    name: "Main Dashboard",
+    layout: "/",
+    path: "dashboard",
+    icon: <MdHome className="h-6 w-6" />,
+    component: <MainDashboard />,
+  },
+  {
+    name: "NFT Marketplace",
+    layout: "/",
+    path: "nft-marketplace",
+    icon: <MdOutlineShoppingCart className="h-6 w-6" />,
+    component: <NFTMarketplace />,
+    secondary: true,
+  },
+  {
+    name: "Data Tables",
+    layout: "/",
+    icon: <MdBarChart className="h-6 w-6" />,
+    path: "data-tables",
+    component: <DataTables />,
+  },
+  {
+    name: "Profile",
+    layout: "/",
+    path: "profile",
+    icon: <MdPerson className="h-6 w-6" />,
+    component: <Profile />,
+  },
+  // {
+  //   name: "Sign In",
+  //   layout: "/auth",
+  //   path: "sign-in",
+  //   icon: <MdLock className="h-6 w-6" />,
+  //   component: <SignIn />,
+  // },
+  // {
+  //   name: "RTL Admin",
+  //   layout: "/rtl",
+  //   path: "rtl",
+  //   icon: <MdHome className="h-6 w-6" />,
+  //   component: <RTLDefault />,
+  // },
+];
+export default routes;
